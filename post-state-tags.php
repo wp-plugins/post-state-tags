a857e70c442e9501453066088b9064ad76e5e2dc
brandbrilliance <brandbrilliance@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-19 15:46:22 +0000

    Version 1.0.1 - Modified dashicon for post archive status to match plugin
    git-svn-id: https://plugins.svn.wordpress.org/post-state-tags/tags/1.0.1@1163456 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- post-state-tags.php
+++ post-state-tags.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Post State Tags
 Plugin URI: https://wordpress.org/plugins/post-state-tags/
-Description: Make your WordPress post state list stand out and add tags (draft, pending, sticky, etc)
+Description: Make your WordPress post state list stand out with colors and color tags (draft, pending, sticky, etc)
 Author: BRANDbrilliance
 Version: 1.0.1
 Author URI: http://www.brandbrilliance.co.za
